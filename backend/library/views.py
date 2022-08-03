1dfd590047a342110237cf0ac017302f844cf022
Aleksey Blagodetelev <darth-maul@mail.ru>
2022-08-03 21:42:38 +0500

    basic rest api

--- backend/library/views.py
+++ backend/library/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Author
+from .serializers import AuthorModelSerializer
+
+
+class AuthorModelViewSet(ModelViewSet):
+    serializer_class = AuthorModelSerializer
+    queryset = Author.objects.all()
