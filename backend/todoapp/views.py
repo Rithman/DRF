69f48d40936d7abdecbf0007f7c2f2b91d5ebff7
Aleksey Blagodetelev <darth-maul@mail.ru>
2022-08-13 13:58:07 +0500

    lesson #4 ready

--- backend/todoapp/views.py
+++ backend/todoapp/views.py
@@ -1,14 +1,33 @@
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.viewsets import ModelViewSet
 
+from .filters import ProjectFilter, TODOFilter
 from .models import TODO, Project
 from .serializers import ProjectSerializer, TODOSerializer
 
 
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+
 class ProjectModelViewSet(ModelViewSet):
+    pagination_class = ProjectLimitOffsetPagination
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
+    filterset_class = ProjectFilter
 
 
 class TODOModelViewSet(ModelViewSet):
+    pagination_class = TODOLimitOffsetPagination
     serializer_class = TODOSerializer
     queryset = TODO.objects.all()
+    filteset_fields = ["Project"]
+    filterset_class = TODOFilter
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
