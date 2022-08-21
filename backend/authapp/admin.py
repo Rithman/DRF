1391041392a8fe0b60acf5dfef7f1cc2cf384cef
Blagodetelev_Aleksey <89443841+Rithman@users.noreply.github.com>
2022-08-21 09:00:02 +0500

    lesson_6 ready (#7)

--- backend/authapp/admin.py
+++ backend/authapp/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from authapp import models
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(models.CustomUser)
+class CustomUserModelAdmin(UserAdmin):
+    list_display = ["id", "username", "email", "is_active", "date_joined"]
+    ordering = ["-date_joined"]
