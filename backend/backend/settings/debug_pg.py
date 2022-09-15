472405c3eaede8d6a7674492b3f720536b431fd0
Blagodetelev_Aleksey <89443841+Rithman@users.noreply.github.com>
2022-09-15 09:37:24 +0500

    lesson_12 (#15)

---
+++ backend/backend/settings/debug_pg.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "todo",
+        "USER": "rithman",
+        "PASSWORD": "32167",
+        "HOST": "127.0.0.1",
+        "PORT": "54328",
+    }
+}
---
+++ backend/backend/settings/production.py
@@ -0,0 +1,12 @@
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "todo",
+        "USER": "rithman",
+        "PASSWORD": "32167",
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
